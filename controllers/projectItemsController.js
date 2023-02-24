b9ada602be4c8ff325e56dafc544ebf5d971cb63
shannon7777 <shannonsimoncherry@gmail.com>
2023-02-25 07:33:05 +0800

    added function to add and remove project item owners

--- controllers/projectItemsController.js
+++ controllers/projectItemsController.js
@@ -1,4 +1,5 @@
 const ProjectItem = require("../models/projectItemModel");
+const User = require("../models/userModel");
 
 const getProjectItems = async (req, res) => {
   try {
@@ -15,9 +16,11 @@ const getProjectItems = async (req, res) => {
 
 const createProjectItem = async (req, res) => {
   console.log(req.body);
+  const { item, deadline } = req.body;
   try {
     const projectItem = await ProjectItem.create({
-      item: req.body.item,
+      item,
+      deadline,
       project_id: req.params.project_id,
     });
     res.status(200).json({ message: `New item created`, projectItem });
@@ -26,4 +29,65 @@ const createProjectItem = async (req, res) => {
   }
 };
 
-module.exports = { getProjectItems, createProjectItem };
+const getOwners = async (req, res) => {
+  const { projectItem_id } = req.params;
+  try {
+    const projectItem = await ProjectItem.findById(projectItem_id)
+      .lean()
+      .exec();
+    const owners = await User.find({ _id: projectItem.owners })
+      .select(["-password", "-tasks", "-refreshToken", "-roles"])
+      .lean()
+      .exec();
+    console.log(owners);
+    res.status(200).json({ owners });
+  } catch (error) {
+    res.sendStatus(400);
+  }
+};
+
+const addOwners = async (req, res) => {
+  const { projectItem_id } = req.params;
+  const ownersArr = req.body;
+  const owners = await User.find({ email: ownersArr }).lean().exec();
+  const ownerIds = owners.map(({ _id }) => _id);
+  const ownerEmails = owners.map(({ email }) => email);
+
+  try {
+    await ProjectItem.findByIdAndUpdate(projectItem_id, {
+      $addToSet: { owners: { $each: [...ownerIds] } },
+    });
+    res
+      .status(200)
+      .json({ message: `${[...ownerEmails]} has been added to task`, owners });
+  } catch (error) {
+    res.status(400).json({ message: `Could not add owner: ${error.message}` });
+  }
+};
+
+const removeOwners = async (req, res) => {
+  const { projectItem_id } = req.params;
+  const owners = req.body;
+  const users = await User.find({ email: owners }).lean().exec();
+  const userIds = users.map(({ _id }) => _id);
+  const userEmails = users.map(({ email }) => email);
+
+  try {
+    await ProjectItem.findByIdAndUpdate(projectItem_id, {
+      $pull: { owners: { $in: [...userIds] } },
+    });
+    res
+      .status(200)
+      .json({ message: `Removed ${[...userEmails]} from this task` });
+  } catch (error) {
+    res.status(400).json({ message: `Could not remove user` });
+  }
+};
+
+module.exports = {
+  getProjectItems,
+  createProjectItem,
+  addOwners,
+  getOwners,
+  removeOwners,
+};
--- routes/projectItemRoutes.js
+++ routes/projectItemRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const {
   getProjectItems,
   createProjectItem,
+  addOwners,
+  getOwners,
+  removeOwners
 } = require("../controllers/projectItemsController");
 
 //   GET ALL PROJECT ITEMS
@@ -10,5 +13,14 @@ router.get("/:project_id", getProjectItems);
 
 // CREATE ITEM
 router.post("/:project_id", createProjectItem);
+
+// Get Owners from each project item
+router.get("/owners/:projectItem_id", getOwners);
+
+// Add owners
+router.post("/owners/:projectItem_id", addOwners);
+
+// Remove Owners
+router.put("/owners/:projectItem_id", removeOwners);
 
 module.exports = router;
